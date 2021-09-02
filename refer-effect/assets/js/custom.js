2e73e768e7e2884635bf117c03e3f4ac00ba3dfb
junaky1 <wjddmswl3712@gmail.com>
2021-09-02 16:32:40 +0900

    확인용

--- refer-effect/assets/js/custom.js
+++ refer-effect/assets/js/custom.js
@@ -7,6 +7,27 @@
         });
     });
     
+    //Code tab menu
+    const vTitle = document.querySelectorAll(".view-title ul li");
+    const vCode = document.querySelectorAll(".view-cont > div");
+
+    vTitle.forEach((element, index) => {
+        element.addEventListener("click", function(){
+
+            vTitle.forEach((el)=>{
+                el.classList.remove("active");
+            });
+            element.classList.add("active");
+
+            vCode.forEach((el)=>{
+                el.style.display = "none";
+            });
+            vCode[index].style.display = "block";
+        });
+    });
+    
+    
+
     //modal
     document.querySelector(".bottom button").addEventListener("click", function(){
         document.querySelector("#modal").classList.remove("hide");
@@ -16,4 +37,3 @@
     document.querySelector(".modal-cont button").addEventListener("click", function(){
         document.querySelector("#modal").classList.add("hide");
     });
-
